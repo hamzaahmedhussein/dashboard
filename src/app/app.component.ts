e9ab37c234c9db3e80e43cdaa11598c91095702c
Hamza Ahmed <hamzahamza142002@gmail.com>
2024-11-16 01:56:16 +0200

    Dashbord V1 Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule,
+     MatButtonToggleModule,MatIconModule,MatProgressBarModule,CommonModule,MatSelectModule,
+     MatCheckboxModule,
+    MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dashboard';
+  activeView: 'graph' | 'list' = 'list';
+
+  toggleView(view: 'graph' | 'list') {
+    this.activeView = view;
+  }
+
 }
